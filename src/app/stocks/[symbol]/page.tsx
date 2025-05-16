aeddb653222ef82b6acdbf4ba4bbc8c08d7cfa3d
joung bueam <jbryu@cloocus.com>
2025-05-16 16:43:21 +0900

    feat: analytics 동적 라우팅

---
+++ src/app/stocks/[symbol]/page.tsx
@@ -0,0 +1,9 @@
+export default function StockPage() {
+  return (
+    <div>
+      <div style={{ height: "100px", width: "100px", background: "red" }}>
+        test
+      </div>
+    </div>
+  );
+}
--- src/widgets/stockSection/ui/StockAnalyticSection.tsx
+++ src/widgets/stockSection/ui/StockAnalyticSection.tsx
@@ -1,14 +1,9 @@
-import { SymbolSearchInput } from "@/features/symbolSearch";
 import { StockAnalyticTable } from "@/features/stockAnalytic";
 
 export default function StockAnalyticSection() {
   return (
     <section>
       <div className="basic-container">
-        <div className="basic-search-box">
-          <SymbolSearchInput />
-        </div>
-
         <div className="basic-result-section">
           <StockAnalyticTable />
         </div>
