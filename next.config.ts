aeb42f29231dbf5a3a38dc5b77b6e8dd1b4fd24f
joung bueam <jbryu@cloocus.com>
2025-05-21 14:54:46 +0900

    chore: next 설정 추가 (console 중복 제거거)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false, //개발 모드 하단의 Next.js 배지/아이콘 제거
   // buildActivity: false, //빌드 상태 표시 없애기 (세부 설정)
+  reactStrictMode: false,
 };
 
 export default nextConfig;
